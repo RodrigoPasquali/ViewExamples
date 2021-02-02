f928433b19874ff33c52ac86b23c4f105807a0b1
josmar torres <josmar.torres3@gmail.com>
2021-02-02 11:10:06 -0500

    delete fragment actualproduct and load info in the main activity

--- android-recyclerview-as-gridview-master/app/src/main/java/com/telynet/viewExamples/View/CarouselAndActualItem/CarouselListProductActivity.java
+++ android-recyclerview-as-gridview-master/app/src/main/java/com/telynet/viewExamples/View/CarouselAndActualItem/CarouselListProductActivity.java
@@ -1,6 +1,8 @@
 package com.telynet.viewExamples.View.CarouselAndActualItem;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentTransaction;
@@ -8,36 +10,32 @@ import androidx.fragment.app.FragmentTransaction;
 import com.telynet.viewExamples.Model.Product;
 import com.telynet.viewExamples.R;
 
-//public class CarouselListProductActivity extends AppCompatActivity implements CarouselFragment.CarouselFragmentListener {
 public class CarouselListProductActivity extends AppCompatActivity implements CarouselFragment.CarouselFragmentListener {
     private CarouselFragment carouselFragment;
-    private ActualProductFragment actualProductFragment;
+    private ImageView productImage;
+    public TextView tvTitle;
+    private TextView tvCode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_carousel_list_product);
 
-        actualProductFragment = new ActualProductFragment();
-
-        getSupportFragmentManager().beginTransaction()
-                .replace(R.id.actual_product_fragment_cotainer, actualProductFragment)
-                .commit();
-
         carouselFragment = new CarouselFragment();
 
         getSupportFragmentManager().beginTransaction()
                 .replace(R.id.carousel_fragment_container, carouselFragment)
                 .commit();
+
+        productImage = findViewById(R.id.iv_product);
+        tvTitle = findViewById(R.id.tv_title);
+        tvCode = findViewById(R.id.tv_code);
     }
 
-
-
     @Override
-    public void onProductSelected(Product product) {
-        actualProductFragment = new ActualProductFragment(product);
-        getSupportFragmentManager().beginTransaction()
-                .replace(R.id.actual_product_fragment_cotainer, actualProductFragment)
-                .commit();
+    public void onProductSelected(Product p) {
+        productImage.setImageResource(p.getImage());
+        tvTitle.setText(p.getTitle());
+        tvCode.setText(p.getCode());
     }
 }
